ba2ae507605e1c662f02197e0c7d34394a5449b1
AuTangen <atangen2406@gmail.com>
2023-07-06 12:12:32 -0400

    added Wildcard to handle crash on page refresh, fixed NaN appearing as grain percentage on page load

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
+
 const express = require('express');
 const session = require('express-session')
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const db = require('./config/connection');
 
